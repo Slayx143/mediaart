b805a40d96b25e8e471954cf2cb7576dbcfa4b8f
Slayx143 <maliziolasimone@gmail.com>
2022-12-18 21:03:56 +0100

    pop up sito

---
+++ main.js
@@ -0,0 +1,17 @@
+//POP UP AL CARICAMENTO DEL SITO 
+
+// Get the modal element
+/* var modal = document.getElementById("modal");
+
+// Show the modal when the page is loaded
+window.addEventListener("load", function() {
+  modal.style.display = "block";
+});
+
+// Add a click event listener to the modal
+modal.addEventListener("click", function() {
+  // Hide the modal when it is clicked
+  modal.style.display = "none";
+});
+
+*/
